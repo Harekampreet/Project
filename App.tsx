d8a4ac70dbaaa41d47bfe013d7bd23a5554224ad
Harekampreet <158284403+Harekampreet@users.noreply.github.com>
2024-01-31 17:35:12 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,59 @@
+
+
+
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import React from 'react';
+import Splash from './Screen/splash';
+import Language from './Screen/language';
+import Login from './Screen/login';
+import ForgotPass from './Screen/ForgotPass';
+import ResetPass from './Screen/ResetPass';
+import SUPhone from './Screen/SUPhone';
+import SUEmail from './Screen/SUEmail';
+import Otpp from './Screen/Otpp';
+import Otpe from './Screen/Otpe';
+import Profile from './Screen/Profile';
+import WatchList from './Screen/WatchList';
+
+const App = () => {
+  return (
+<WatchList/> 
+//   <Profile />
+   //  <Otpe/>
+// {/* <Otpp/> */}
+
+
+
+  //  <SUEmail/>
+//  <SUPhone/>  
+/* <Language/> */
+
+     
+    // <ResetPass/>
+    // <ForgotPass/>
+   // <SafeAreaView>
+   //    <ScrollView>
+    //    <Login />
+    //   </ScrollView>
+               // </SafeAreaView>
+  );
+};
+
+export default App;
+// import React from 'react';
+// import {NavigationContainer} from '@react-navigation/native';
+// import Navigation from './HomeStack/Navigation';
+
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <Navigation />
+//     </NavigationContainer>
+//   );
+// };
+
+// export default App;
+
+
+
+
